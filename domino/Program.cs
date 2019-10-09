bffc0c05c3ac0b0a55df283af27ae9c8ce63ddbf
David Armando Paez Renteria <davidpaez662@gmail.com>
2019-10-08 19:40:09 -0700

    Domino

---
+++ domino/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace domino
+{
+    class Domino
+    {
+        public readonly int LadoA;
+        public readonly int LadoB;
+
+        public Domino(int LA, int LB)
+        {
+            LadoA= LA;
+            LadoB= LB;
+
+        }
+        public static int operator +(Domino a, Domino b)
+        {
+            return (a.LadoA+a.LadoB+b.LadoA+b.LadoB);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Domino (2,0);
+            var b = new Domino (4,1);
+            Console.WriteLine(a+b);
+
+        }
+    }
+}
