353660d798be31ad46496332bbb21cc96be0de1a
David Armando Paez Renteria <davidpaez662@gmail.com>
2019-10-16 12:54:42 -0700

    Beta

---
+++ Figurass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Figurass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
